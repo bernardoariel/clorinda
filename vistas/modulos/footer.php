25c8d7e3c68e66f04c0173cd83f5b657f67d6f68
Ariel Bernardo <arielbernardo@hotmail.com>
2021-08-30 11:12:58 -0300

    ahora si

---
+++ vistas/modulos/footer.php
@@ -0,0 +1,35 @@
+<style>
+
+
+.float{
+
+  width:30px;
+  height:30px;
+  background-color:#25d366;
+  color:#FFF;
+  border-radius:50px;
+  text-align:center;
+  font-size:20px;
+  box-shadow: 2px 2px 3px #999;
+  z-index:100;
+}
+.float:hover {
+  text-decoration: none;
+  color: #25d366;
+  background-color:#fff;
+}
+
+.my-float{
+  line-height: 30px;
+}
+</style>
+
+<footer class="main-footer">
+	
+	<strong>Copyright &copy; 2018 <a href="http://www.arielbernardo.com.ar" target="_blank">ARIEL BERNARDO</a>.</strong>
+	Todos los derechos reservados.
+
+	<a href="https://api.whatsapp.com/send?phone=5493704299434" class="float pull-right" target="_blank">
+		<i class="fa fa-whatsapp my-float"></i>
+	</a>
+</footer>
